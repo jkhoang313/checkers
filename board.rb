fcaaa1d3a285c872fa2dc644c32835d33373c6ec
jkhoang313 <jkhoang313@gmail.com>
2016-12-20 09:48:53 -0500

    refactored

--- board.rb
+++ board.rb
@@ -1,47 +1,47 @@
 require 'pry'
 
 class Board
-  attr_reader :board, :players, :game_over
+  attr_reader :board, :player_one, :player_two, :game_over
 
   def initialize
     @board = Array.new(8){Array.new(8)}
-    @players = [Player.new("Black"), Player.new("White")]
+    @player_one = Player.new("Black")
+    @player_two = Player.new("White")
 
-    @players[0].pieces.each do |piece|
-      @board[piece.hor][piece.vert] = "Black"
-    end
+    add_pieces_to_board(player_one)
+    add_pieces_to_board(player_two)
 
-    @players[1].pieces.each do |piece|
-      @board[piece.hor][piece.vert] = "White"
-    end
     draw_board
     run
   end
 
+  def add_pieces_to_board(player)
+    player.pieces.each {|piece| board[piece.hor][piece.vert] = piece}
+  end
+
   def draw_board
-    @board.each_with_index do |rows, row|
-      print "#{row+1}|"
-      rows.each_with_index do |position, column|
-        if @board[row][column] == nil
-          print "__|"
-        else
-          if @board[row][column] == "Black"
-            icon = "\u265F".encode('utf-8')
-          elsif @board[row][column] == "White"
-            icon = "\u2659".encode('utf-8')
-          end
-          print "_#{icon}|"
-        end
+    board.each.with_index(1) do |rows, row|
+      print "#{row}|"
+      rows.each.with_index(1) do |position, column|
+        print position.nil? ? "__|" : "_#{get_icon(position.side)}|"
       end
       print "\n"
     end
     print "   1  2  3  4  5  6  7  8" + "\n"
   end
 
+  def get_icon(side)
+    if side == "Black"
+      icon = "\u265F".encode('utf-8')
+    elsif side == "White"
+      icon = "\u2659".encode('utf-8')
+    end
+  end
+
   def run
-    # until @game_over
-      turn(@players[0])
-      turn(@players[1])
+    # until game_over?
+      turn(player_one)
+      turn(player_two)
     # end
   end
 
@@ -51,39 +51,17 @@ class Board
     row = ask_for_input("row") - 1
     column = ask_for_input("column") - 1
 
-    position = @board[row][column]
-
-    if !position.empty? && position == turn_player.side
+    if check_valid_position?(row, column, turn_player.side)
       puts "Where do you want to move (right or left)?"
       direction = gets.strip
       move(direction, row, column, turn_player.side)
     end
   end
 
-  def move(direction, row, column, turn_player)
-    old_position = @board[row][column]
-
-    if turn_player == "Black"
-      if direction == "right"
-        @board[row-1][column+1] = old_position
-      elsif direction == "left"
-        @board[row-1][column-1] = old_position
-      end
-    elsif turn_player == "White"
-      if direction == "right"
-        @board[row+1][column+1] = old_position
-      elsif direction == "left"
-        @board[row+1][column-1] = old_position
-      end
-    end
-
-    @board[row][column] = nil
-    draw_board
-  end
-
   def ask_for_input(position)
     puts "Enter the #{position} of the piece you want to move."
     input = gets.strip.to_i
+
     until input.between?(1, 8)
       puts "Please enter a valid input"
       input = gets.strip.to_i
@@ -91,4 +69,32 @@ class Board
     input
   end
 
+  def check_valid_position?(row, column, side)
+    position = board[row][column]
+    !position.nil? && position.side == side
+  end
+
+  def move(direction, row, column, turn_player)
+    piece = board[row][column]
+
+    if turn_player == "Black"
+      if direction == "right"
+        board[row-1][column+1] = piece
+      elsif direction == "left"
+        board[row-1][column-1] = piece
+      end
+    elsif turn_player == "White"
+      if direction == "right"
+        board[row+1][column+1] = piece
+      elsif direction == "left"
+        board[row+1][column-1] = piece
+      end
+    end
+
+    board[row][column] = nil
+    draw_board
+  end
+
+  def game_over?
+  end
 end
--- player.rb
+++ player.rb
@@ -4,13 +4,17 @@ class Player
   def initialize(side)
     @side = side
     if side == "White"
-      @pieces = [Piece.new(0, 0, 1), Piece.new(0, 2, 1), Piece.new(0, 4, 1), Piece.new(0, 6, 1),
-        Piece.new(1, 1, 1), Piece.new(1, 3, 1), Piece.new(1, 5, 1), Piece.new(1, 7, 1),
-        Piece.new(2, 0, 1), Piece.new(2, 2, 1), Piece.new(2, 4, 1), Piece.new(2, 6, 1)]
+      @pieces = [
+        Piece.new(0, 0, "White"), Piece.new(0, 2, "White"), Piece.new(0, 4, "White"), Piece.new(0, 6, "White"),
+        Piece.new(1, 1, "White"), Piece.new(1, 3, "White"), Piece.new(1, 5, "White"), Piece.new(1, 7, "White"),
+        Piece.new(2, 0, "White"), Piece.new(2, 2, "White"), Piece.new(2, 4, "White"), Piece.new(2, 6, "White")
+      ]
     elsif side == "Black"
-      @pieces = [Piece.new(7, 1, 2), Piece.new(7, 3, 2), Piece.new(7, 5, 2), Piece.new(7, 7, 2),
-        Piece.new(6, 0, 2), Piece.new(6, 2, 2), Piece.new(6, 4, 2), Piece.new(6, 6, 2),
-        Piece.new(5, 1, 2), Piece.new(5, 3, 2), Piece.new(5, 5, 2), Piece.new(5, 7, 2)]
+      @pieces = [
+        Piece.new(7, 1, "Black"), Piece.new(7, 3, "Black"), Piece.new(7, 5, "Black"), Piece.new(7, 7, "Black"),
+        Piece.new(6, 0, "Black"), Piece.new(6, 2, "Black"), Piece.new(6, 4, "Black"), Piece.new(6, 6, "Black"),
+        Piece.new(5, 1, "Black"), Piece.new(5, 3, "Black"), Piece.new(5, 5, "Black"), Piece.new(5, 7, "Black")
+      ]
     end
   end
 end
